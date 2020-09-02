b9a21f83bc322766c95b6d7f733aff1bf3480d79
Nicolas-Chopin <chopin.nico@gmail.com>
2020-09-02 13:37:27 +0200

    API+AJAX for all scores +/- OK

--- public/js/app.js
+++ public/js/app.js
@@ -10,9 +10,15 @@ let app = {
         let url = new URL(document.URL);
         let id = url.pathname.split('/')[2];
 
-        let aScoreFetched = document.getElementById("a-score").innerHTML;
         
-        // on cible
+        
+        //--------------------------------------------------------------------
+        // Plus & Minus methods for teams' scores
+        //--------------------------------------------------------------------
+        // fetching current scores
+        let aScoreFetched = document.getElementById("a-score").innerHTML;
+        let bScoreFetched = document.getElementById("b-score").innerHTML;
+        // target + event resolve for +1 point to A team
         let plusA = document.getElementById("plus-a-team");
         plusA.addEventListener("click", function () {
           let fetchOptions = {
@@ -23,6 +29,42 @@ let app = {
           fetch(app.apiBaseURL + 'api/session/'+id+'/plusonea', fetchOptions)
           .then(app.convertJSONtoJS)
           .then(document.getElementById("a-score").innerHTML = ++aScoreFetched);
+        },);
+        // target + event resolve for +1 point to B team
+        let plusB = document.getElementById("plus-b-team");
+        plusB.addEventListener("click", function () {
+          let fetchOptions = {
+          method: 'POST',
+          mode: 'cors',
+          cache: 'no-cache'
+          };
+          fetch(app.apiBaseURL + 'api/session/'+id+'/plusoneb', fetchOptions)
+          .then(app.convertJSONtoJS)
+          .then(document.getElementById("b-score").innerHTML = ++bScoreFetched);
+        },);
+        // target + event resolve for -1 point to A team
+        let minusA = document.getElementById("minus-a-team");
+        minusA.addEventListener("click", function () {
+          let fetchOptions = {
+          method: 'POST',
+          mode: 'cors',
+          cache: 'no-cache'
+          };
+          fetch(app.apiBaseURL + 'api/session/'+id+'/minusonea', fetchOptions)
+          .then(app.convertJSONtoJS)
+          .then(document.getElementById("a-score").innerHTML = --aScoreFetched);
+        },);
+        // target + event resolve for -1 point to B team
+        let minusB = document.getElementById("minus-b-team");
+        minusB.addEventListener("click", function () {
+          let fetchOptions = {
+          method: 'POST',
+          mode: 'cors',
+          cache: 'no-cache'
+          };
+          fetch(app.apiBaseURL + 'api/session/'+id+'/minusoneb', fetchOptions)
+          .then(app.convertJSONtoJS)
+          .then(document.getElementById("b-score").innerHTML = --bScoreFetched);
         },);
     },
     
--- src/Controller/Api/ApiScoreController.php
+++ src/Controller/Api/ApiScoreController.php
@@ -22,7 +22,6 @@ class ApiScoreController extends AbstractController
         if ($currentSession === null) {
             return $this->json(['message' => 'Session non existante.'], Response::HTTP_NOT_FOUND);
         }
-
         
         $currentScore = $currentSession->getATeamScore();
         $scoreA = $currentSession->setATeamScore($currentScore+1);
@@ -32,5 +31,74 @@ class ApiScoreController extends AbstractController
         $entityManager->flush($scoreA);
 
         return $this->json('1 point gagné', Response::HTTP_OK);       
+    }
+
+    /**
+     * Add a point to B Team 
+     * 
+     * @Route("/api/session/{id<\d+>}/plusoneb", name="api_plusoneb", methods={"POST"})
+     */
+    public function plusOneB($id, SessionRepository $sessionRepository)
+    {
+        $currentSession = $sessionRepository->findOneBy(['id' => $id]);
+
+        if ($currentSession === null) {
+            return $this->json(['message' => 'Session non existante.'], Response::HTTP_NOT_FOUND);
+        }
+        
+        $currentScore = $currentSession->getBTeamScore();
+        $scoreB = $currentSession->setBTeamScore($currentScore+1);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($scoreB);
+        $entityManager->flush($scoreB);
+
+        return $this->json('1 point gagné', Response::HTTP_OK);       
+    }
+
+    /**
+     * Minus a point to A Team 
+     * 
+     * @Route("/api/session/{id<\d+>}/minusonea", name="api_minusonea", methods={"POST"})
+     */
+    public function minusOneA($id, SessionRepository $sessionRepository)
+    {
+        $currentSession = $sessionRepository->findOneBy(['id' => $id]);
+
+        if ($currentSession === null) {
+            return $this->json(['message' => 'Session non existante.'], Response::HTTP_NOT_FOUND);
+        }
+        
+        $currentScore = $currentSession->getATeamScore();
+        $scoreA = $currentSession->setATeamScore($currentScore-1);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($scoreA);
+        $entityManager->flush($scoreA);
+
+        return $this->json('1 point perdu', Response::HTTP_OK);       
+    }
+
+    /**
+     * Minus a point to B Team 
+     * 
+     * @Route("/api/session/{id<\d+>}/minusoneb", name="api_minusoneb", methods={"POST"})
+     */
+    public function minusOneB($id, SessionRepository $sessionRepository)
+    {
+        $currentSession = $sessionRepository->findOneBy(['id' => $id]);
+
+        if ($currentSession === null) {
+            return $this->json(['message' => 'Session non existante.'], Response::HTTP_NOT_FOUND);
+        }
+        
+        $currentScore = $currentSession->getBTeamScore();
+        $scoreB = $currentSession->setBTeamScore($currentScore-1);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($scoreB);
+        $entityManager->flush($scoreB);
+
+        return $this->json('1 point perdu', Response::HTTP_OK);       
     }
 }
