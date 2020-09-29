50e545ed914ee6f6f8041c8b321546f6fe9cc9f0
Nicolas-Chopin <chopin.nico@gmail.com>
2020-09-29 15:56:22 +0200

    fix migrations issue

---
+++ src/Migrations/Version20200929135040.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200929135040 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        //$this->addSql('ALTER TABLE answer ADD CONSTRAINT FK_DADD4A25613FECDF FOREIGN KEY (session_id) REFERENCES session (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        //$this->addSql('ALTER TABLE answer DROP FOREIGN KEY FK_DADD4A25613FECDF');
+    }
+}
